2d9ed51c28f632b06561925d776746860d35d8ce
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-01 15:00:28 +0530

    Packages updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,14 @@ const express  = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use("/hello", (req, res) => {
+    res.send("Welcome to the Node.js Server");
+})
+
+app.use("/hi", (re, res) => {
+    res.send("Hi there!");
+})
+
 app.get('/', (req, res) => {
     console.log("Hello devs");
 })
