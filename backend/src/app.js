c2a42d63947c36692c1dc2a93f672e5c73284421
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-03 06:41:52 +0530

    Playing with mutiple route handlers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,42 @@
-const express  = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/user", (req, res) => {
-    res.send("Data sucessfully fetched!");
-})
+// app.use(
+//   "/",
+//   [(req, res, next) => {
+//     next();
+//     res.send("1st Handler");
+//   },
+//   (req, res, next) => {
+//     res.send("2nd Handler");
+//     next();
+//   }],
+//   (req, res, next) => {
+//     next();
+//     res.send("3rd Handler");
+//   },
+//   (req, res, next) => {
+//     res.send("4th Handler");
+//     next();
+//   }
+// );
 
-app.use('/', (req, res) => {
-    res.send("Dashboard")
-})
+const hr1 = (req, res, next) => {
+    next();
+    res.send("1st handler")
+}
+const hr2 = (req, res, next) => {
+    res.send("2nd handler")
+    next();
+}
+const hr3 = (req, res, next) => {
+    next();
+    res.send("3rd handler")
+}
 
+app.use("/user", [hr1, hr2], hr3);
 
-app.listen(port , () => {
-    console.log("Server is listening on port " + port);
-})
+app.listen(port, () => {
+  console.log("Server is listening on port " + port);
+});
