193c2a6e7374e07d503a7a30fe88eb5169451fbf
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-03 18:27:59 +0530

    Created mongoose schema and upload data in db by post api

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,17 +2,33 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
 const port = process.env.PORT || 3000;
+const User = require("./models/user");
+
+// Creating a post api
+app.post("/signup", async (re, res) => {
+  // Creating a new instance of the user model
+  const user = new User({
+    firstName: "Ekant",
+    lastName: "Verma",
+    email: "Ekant@gmail.com",
+    password: "Ekant@",
+    age: 18,
+    gender: "Male",
+  });
+  try {
+    await user.save();
+    res.send("User data saved successfully");
+  } catch (err) {
+    console.error("Error while saving user data:", err);
+    res.status(500).send("Server Error");
+  }
+});
 
 connectDB()
-.then(() => {
+  .then(() => {
     console.log("Database Connected successfully!");
     app.listen(port, () => {
-      console.log("Server listening on port " + port)
-    })
-})
-.catch(err => console.log("Database ConnectFailed!"));
-
-
-
-
-
+      console.log("Server listening on port " + port);
+    });
+  })
+  .catch((err) => console.log("Database Connection Failed!", err));
