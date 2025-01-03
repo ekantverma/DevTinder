fba2047019c2963f071fdc9dcb7c7c152c48ea7f
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-03 11:34:32 +0530

    Error Handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,20 +4,42 @@ const port = process.env.PORT || 3000;
 const {isAuth, userAuth} = require("./middlewares/auth");
 
 // Middleware when we request any admin api the middleware will be called for all the routes get patch put requests.
-app.use("/admin", isAuth);
-app.use("/user", userAuth);
+// app.use("/admin", isAuth);
+// app.use("/user", userAuth);
 
-app.get("/user", userAuth, (req, res) => {
-  res.send("User data sended!")
+// app.get("/user", userAuth, (req, res) => {
+//   res.send("User data sended!")
+// })
+
+// app.get("/admin/getData", (req, res) => {
+//   res.send("Data send to admin!")
+// })
+
+// app.get("/admin/deleteData", (req, res) => {
+//   res.send("Data deleted!")
+// })
+
+// Error Handling 
+app.get("/userData", (req, res) => {
+  // If this route throw an error
+  try{
+    // Logic
+    throw new error("Data not found");
+  res.send("Data fetched!")
+  } catch(err){
+    res.status(500).send("Something went wrong, contact support team!")
+  }
 })
 
-app.get("/admin/getData", (req, res) => {
-  res.send("Data send to admin!")
+// Handling the error
+app.use("/", (err, req, res, next) => {
+  if(err){
+    res.status(500).send("Something went wrong");
+  }
 })
 
-app.get("/admin/deleteData", (req, res) => {
-  res.send("Data deleted!")
-})
+
+// The order is very important of routes
 
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
