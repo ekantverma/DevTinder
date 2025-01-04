f51caf89180bc9aa3702e6d52e6f75f816783b3c
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-04 21:36:21 +0530

    Get apis for fetch feed and users from database

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,6 +20,27 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+// Get api to find user by email from database
+app.get("/user", async (req, res) => {
+  try{
+    const email = req.body.email;
+    const users = await User.find({email : email});
+    res.send(users);
+  } catch(err){
+    res.status(404).send("User not found");
+  }
+})
+
+// Feed Api get all user from the database
+app.get("/feed", async (req, res) => {
+  try{
+    const feed = await User.find();
+    res.send(feed);
+  } catch(err){
+    res.status(404).send("Feed is empty");
+  }
+})
+
 connectDB()
   .then(() => {
     console.log("Database Connected successfully!");
