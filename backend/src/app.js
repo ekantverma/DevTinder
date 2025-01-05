77df4e7938168b7af5496bb8532ef369b26484f5
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-05 17:02:51 +0530

    Update the shema and add validations

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,50 +22,51 @@ app.post("/signup", async (req, res) => {
 
 // Get api to find user by email from database
 app.get("/user", async (req, res) => {
-  try{
+  try {
     const email = req.body.email;
-    const users = await User.find({email : email});
+    const users = await User.find({ email: email });
     res.send(users);
-  } catch(err){
+  } catch (err) {
     res.status(404).send("User not found");
   }
-})
+});
 
 // Feed Api get all user from the database
 app.get("/feed", async (req, res) => {
-  try{
+  try {
     const feed = await User.find();
     res.send(feed);
-  } catch(err){
+  } catch (err) {
     res.status(404).send("Feed is empty");
   }
-})
+});
 
 // Delete user from database
 app.delete("/user", async (req, res) => {
-  try{
+  try {
     const userId = res.body;
     const user = await User.findByIdAndDelete(userId);
     res.send("User deleted successfully!");
-  }
-  catch(err) {
+  } catch (err) {
     res.status(404).send("Something went wrong");
   }
-})
+});
 
 // Update user in database
 app.patch("/user", async (req, res) => {
   const userId = req.body.userId;
   const data = req.body;
-  console.log(data + userId);
-  try{
-    const updatedUser = await User.findByIdAndUpdate(userId, data, returnDocument = "after");
-    runValidators = true;
+  console.log(data);
+  try {
+    const updatedUser = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
     console.log(updatedUser);
-  } catch(err) {
+  } catch (err) {
     res.status(400).send("Something went wrong.");
   }
-})
+});
 
 connectDB()
   .then(() => {
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,47 +1,69 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: { 
-    type: String, 
-    required: true 
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+          throw new Error("Invalid email format");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+            v
+          );
+        },
+        message: (props) =>
+          `${props.value} is not a valid password! Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.`,
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error(
+            "Gender should be either'male', 'female', or 'others'"
+          );
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://i.pinimg.com/736x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg",
+    },
+    about: {
+      type: String,
+      default: "This is default about!",
+    },
+    skills: [String],
   },
-  lastName: { 
-    type: String 
-  },
-  email: { 
-    type: String, 
-    lowercase: true,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  password: { 
-    type: String, 
-    required: true 
-  },
-  age: { 
-    type: Number,
-    min: 18
-  },
-  gender: { 
-    type: String,
-    validate(value){
-      if(!["male", "female", "others"].includes(value)){
-        throw new Error("Gender should be either'male', 'female', or 'others'")
-      }
-    }
-  },
-  photoUrl: {
-    type: String,
-    default: 'https://i.pinimg.com/736x/0a/a8/58/0aa8581c2cb0aa948d63ce3ddad90c81.jpg'
-  }, 
-  about: {
-    type: String,
-    default: "This is default about!"
-  },
-  skills: [String]
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
