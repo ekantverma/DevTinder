6d37d328603f772138a6dad802388b2ec84975d2
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-05 08:35:20 +0530

    Add patch api in app to update user from database

--- backend/src/app.js
+++ backend/src/app.js
@@ -53,7 +53,17 @@ app.delete("/user", async (req, res) => {
   }
 })
 
-
+// Update user in database
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try{
+    const updatedUser = await User.findByIdAndUpdate(userId, data, returnDocument = "after");
+    console.log(updatedUser);
+  } catch(err) {
+    res.status(400).send("Something went wrong.");
+  }
+})
 
 connectDB()
   .then(() => {
