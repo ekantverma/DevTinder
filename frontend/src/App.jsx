5762d5c0effcb29101448e9c7e5b127bc78f420e
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-14 16:18:29 +0530

    Added Navbar and footer components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Body from "./Body.jsx";
+import Login from "./components/Login.jsx";
+import Signup from "./components/Signup.jsx";
+import Profile from "./components/Profile.jsx";
 
 export default function App() {
   return (
     <>
       <BrowserRouter basename="/">
         <Routes>
-          <Route path="/" element={<Body />} />
+          <Route path="/" element={<Body />}>
+           <Route path="/login" element={<Login />} />
+           <Route path="/signup" element={<Signup />} />
+           <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
