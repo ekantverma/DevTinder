7c8cb7e4d52a87853683a6b8dc53207db57fa03c
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-14 10:47:35 +0530

    Frontend initialized

---
+++ frontend/eslint.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true, 
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    ...react.configs['jsx-runtime'].rules,
+    ...reactHooks.configs.recommended.rules,
+    'react/jsx-no-target-blank': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-pink-500">DevTinder</h1>
+    </div>
+  );
+}
+
+export default App;
