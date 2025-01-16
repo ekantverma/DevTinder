b4519066c136c3b0022b3ceb97bb379e41e15849
Ekant Verma <ekantvermasolanki@gmail.com>
2025-01-16 11:18:59 +0530

    Login faeture updated

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import Navbar from './Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Footer from './Footer';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try{
+    const res = await axios.get("http://localhost:3000/profile/view", {
+      withCredentials: true,
+    });
+    dispatch(addUser(res.data));
+  } catch(err) {
+    if(err.status == 401){
+      navigate("/login");
+    }
+  }
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar/>
--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Feed = () => {
   return (
     <div>
-      Feed
+      <h1>Feed Cards</h1>
     </div>
   )
 }
