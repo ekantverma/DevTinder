6f15bfaa501af0c149f49139b97859f24cd364cf
Ekant Verma <ekantvermasolanki@gmail.com>
2025-02-06 13:29:40 +0530

    UPDATED

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 require('dotenv').config();
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://devtinder.vercel.app"],
   credentials: true,
 }));
 app.use(express.json());
